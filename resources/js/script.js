37fd6e724f9cf79d60449544662878f290d162c4
Yoan Latchere <latchere.y@gmail.com>
2021-11-24 17:10:28 +0100

    correction change-color.js for Azuriom < 3.7.0

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,16 +1,18 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     "use strict";
-    new Glide('.glide_staff',{
-        type: 'carousel',
-        startAt: 0,
-        perView: 3,
-        breakpoints:{
-            992: {
-                perView: 2
-            },
-            768: {
-                perView: 1
+    if (document.getElementsByClassName('glide_staff')) {
+        new Glide('.glide_staff', {
+            type: 'carousel',
+            startAt: 0,
+            perView: 3,
+            breakpoints: {
+                992: {
+                    perView: 2
+                },
+                768: {
+                    perView: 1
+                }
             }
-        }
-    }).mount()
+        }).mount()
+    }
 });
